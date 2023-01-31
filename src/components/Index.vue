8bd0b617e7a2889848091202e49be8852728a92d
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-31 23:40:16 +0100

    add pagination

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,4 +1,68 @@
 <script setup>
+
+import { ref } from 'vue';
+import PaginationBar from './PaginationBar.vue';
+
+// Data
+
+const beersPerPage = 18;
+const defaultIbuFilterType = 'ibu_gt';
+
+// Reactive data
+
+const apiEndpoint = "https://api.punkapi.com/v2/beers";
+const beerName = ref("");
+const ibuFilterType = ref(defaultIbuFilterType);
+const ibuFilterValue = ref("");
+const beers = ref([]);
+const currentPage = ref(1);
+
+
+
+/**
+ * Reset the filters.
+ */
+async function reset() {
+    beerName.value = '';
+    ibuFilterValue.value = '';
+    ibuFilterType.value = defaultIbuFilterType;
+    getBeers();
+}
+
+/**
+ * Get beers from the API.
+ */
+async function getBeers(page = 1) {
+
+    currentPage.value = page;
+
+    let queryParams = {
+        per_page: beersPerPage,
+        page: currentPage.value,
+    };
+
+    beerName.value ? queryParams.beer_name = beerName.value : '';
+    ibuFilterValue.value ? queryParams[ibuFilterType.value] = ibuFilterValue.value : '';
+
+
+
+    let axiosResponse = await axios({
+        method: 'GET',
+        url: apiEndpoint,
+        params: queryParams,
+    })
+        .then((response) => {
+            console.log(response)
+            beers.value = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+
+        });
+}
+
 </script>
 
 <template>
@@ -13,22 +77,24 @@
                                 <a class="navbar-brand">Beer index</a>
                             </div>
                             <div class="col-12 col-md-3 ps-0 pe-0 mb-3 mb-md-0">
-                                <input class="form-control" type="search" placeholder="Name">
+                                <input class="form-control" type="text" placeholder="Name" v-model="beerName">
                             </div>
                             <div class="col-12 col-md-3 ps-0 ps-md-3 col-lg-2 pe-0 mb-3 mb-md-0">
                                 <div class="input-group">
-                                    <input class="form-control w-50" type="search" placeholder="IBU">
-                                    <select class="form-select w-50">
-                                        <option selected value="">GT</option>
-                                        <option value="">LT</option>
+                                    <input class="form-control w-50" type="number" placeholder="IBU" step="0.5" min="0"
+                                        max="100" v-model="ibuFilterValue">
+                                    <select class="form-select w-50" v-model="ibuFilterType">
+                                        <option value="ibu_gt">GT</option>
+                                        <option value="ibu_lt">LT</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-6 ps-0 ps-md-3 col-md-auto pe-0">
-                                <button class="btn btn-success w-100" type="submit">Search</button>
+                                <button class="btn btn-success w-100" type="submit"
+                                    v-on:click="getBeers()">Search</button>
                             </div>
                             <div class="col-6 ps-sm-3 col-md-auto pe-0">
-                                <button class="btn btn-secondary w-100" type="button">Reset</button>
+                                <button class="btn btn-secondary w-100" type="button" v-on:click="reset()">Reset</button>
                             </div>
                         </div>
                     </ul>
@@ -38,22 +104,23 @@
         <main class="mt-4">
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
-                    <div class="col">
+                    <div class="col" v-for="beer in beers" :key="beer.id">
                         <div class="card h-100">
                             <div class="card-body">
-                                <h5 class="card-title text-center">Beer name</h5>
+                                <h5 class="card-title text-center text-truncate">{{ beer.name }}</h5>
                                 <div class="row mx-auto">
                                     <div class="col-12 col-md-5 col-xl-6">
-                                        IBU: 100
+                                        IBU: {{ beer.ibu }}
                                     </div>
                                     <div class="col-12 col-md-7 col-xl-6">
-                                        FoodPairing: 100
+                                        FoodPairing: {{ beer.food_pairing.length }}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <PaginationBar v-on:paginate="getBeers" :current-page="currentPage" />
             </div>
         </main>
     </div>
---
+++ src/components/PaginationBar.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+// Props
+
+const props = defineProps({
+    currentPage: Number,
+});
+
+// Emits
+
+const emit = defineEmits(['paginate']);
+
+</script>
+
+<template>
+    <div class="row mt-4">
+        <div class="col-12">
+            <nav>
+                <ul class="pagination justify-content-end m-0 d-flex flex-wrap">
+
+                    <!-- Previous Page Link -->
+
+                    <li class="page-item" :class="{ disabled: props.currentPage == 1 }">
+                        <a class="page-link" v-on:click="emit('paginate', props.currentPage - 1)"
+                            rel="prev">&lsaquo;</a>
+                    </li>
+
+                    <!-- Pagination Elements -->
+
+                    <template v-for="pageNumber in 4" :value="pageNumber" :key="pageNumber">
+
+                        <li v-if="pageNumber >= 0 && pageNumber == props.currentPage" class="page-item active"><span
+                                class="page-link">{{
+                                    pageNumber
+                                }}</span></li>
+                        <li v-else-if="pageNumber >= 0 && pageNumber != props.currentPage" class="page-item">
+                            <a class="page-link pointer" v-on:click="emit('paginate', pageNumber)">{{
+                                pageNumber
+                            }}</a>
+                        </li>
+
+                    </template>
+
+                    <!-- Next Page Link -->
+
+                    <li class="page-item" :class="{ disabled: props.currentPage == 4 }">
+                        <a class="page-link" v-on:click="emit('paginate', props.currentPage + 1)"
+                            rel="next">&rsaquo;</a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
